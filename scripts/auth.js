b26b4a7ea65e0302d7e4ed10180a43c3753f2b0e
yaniswhy <slimaniyanis09@gmail.com>
2024-07-15 21:32:34 +0100

    loginadded

---
+++ scripts/auth.js
@@ -0,0 +1,13 @@
+document.getElementById('loginBtn').addEventListener('click', function() {
+  document.getElementById('loginOverlay').style.display = 'block';
+});
+
+document.getElementById('closeBtn').addEventListener('click', function() {
+  document.getElementById('loginOverlay').style.display = 'none';
+});
+
+window.addEventListener('click', function(event) {
+  if (event.target == document.getElementById('loginOverlay')) {
+      document.getElementById('loginOverlay').style.display = 'none';
+  }
+});
