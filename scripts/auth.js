a6e9f90e336824d4478a06c3eea4c9e2fc5d3672
yaniswhy <slimaniyanis09@gmail.com>
2024-07-15 23:08:56 +0100

    lmaio

--- scripts/auth.js
+++ scripts/auth.js
@@ -6,6 +6,9 @@ document.getElementById('closeBtn').addEventListener('click', function() {
   document.getElementById('loginOverlay').style.display = 'none';
 });
 
+document.getElementById('inv').addEventListener('click', function() {
+  document.getElementById('loginOverlay').style.display = 'none';
+});
 window.addEventListener('click', function(event) {
   if (event.target == document.getElementById('loginOverlay')) {
       document.getElementById('loginOverlay').style.display = 'none';
